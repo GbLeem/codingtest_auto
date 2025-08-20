42ef94a3f32bfc7f090d04575e05c64ad26aa945
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2025-08-20 20:34:13 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2492 ms, Memory: 3524 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -5,19 +5,22 @@ using namespace std;
 
 int n, m;
 vector<int> adj[10'002];
-int vis[10'002];
-int depth[10'002];
-
+bool vis[10'002];
+int cnt = 0;
+vector<int> ans;
+int maxCnt = -1;
 
 void dfs(int cur)
 {
 	vis[cur] = 1;
-	depth[cur]++;
+
 	for (int nxt : adj[cur])
 	{
-		if (vis[nxt])
-			continue;
-		dfs(nxt);
+		if (!vis[nxt])
+		{
+			cnt++;
+			dfs(nxt);
+		}
 	}
 }
 
@@ -27,26 +30,34 @@ int main()
 	cin.tie(0);
 
 	cin >> n >> m;
-
 	for (int i = 0; i < m; ++i)
 	{
 		int u, v;
 		cin >> u >> v;
-
-		adj[u].push_back(v);
+		//adj[u].push_back(v);
+		adj[v].push_back(u);
 	}
 
 	for (int i = 1; i <= n; ++i)
 	{
-		fill(vis +1, vis + n + 1, 0);
+		fill(vis, vis + 10'002, 0);
+		cnt = 0;
 		dfs(i);
+		if (maxCnt < cnt)
+		{
+			ans.clear();
+			ans.push_back(i);
+			maxCnt = cnt;
+		}
+		else if (maxCnt == cnt)
+		{
+			ans.push_back(i);
+		}
 	}
 
-	int answer = *max_element(depth + 1, depth + n + 1);
-
-	for (int i = 1; i <= n; ++i)
+	sort(ans.begin(), ans.end());
+	for (const auto& a : ans)
 	{
-		if (answer == depth[i])
-			cout << i << " ";
+		cout << a << " ";
 	}
 }
