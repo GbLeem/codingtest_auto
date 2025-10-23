496d6b3c0f03fbb39eae04b54738f368465ea961
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2025-10-23 21:09:32 +0900

    [Silver II] Title: 보이는 점의 개수, Time: 24 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.cc
+++ 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.cc
@@ -18,17 +18,20 @@ int main()
 	cin.tie(0);
 
 	cin >> c;
+	
+	board[1] = 3;
 
-	for (int i = 0; i <= 1000; ++i)
+	for (int i = 2; i <= 1000; ++i)
 	{
-		for (int j = 0; j <= 1000; j++)
+		int cnt = 0;
+
+		for (int j = 1; j < i; j++)
 		{
 			if (gcd(i, j) == 1)
-				board[max(i, j)] += 1;
+				cnt++;
 		}
+		board[i] = board[i - 1] + cnt * 2;
 	}
-	for (int i = 1; i <= 1000; ++i)
-		board[i] += board[i - 1];
 	while (c--)
 	{
 		cin >> n;
